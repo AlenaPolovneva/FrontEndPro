87cfc7736696084a86925187665e0d58c47aa438
Your Name <you@example.com>
2025-02-19 22:34:20 +0200

    lec3_hw3

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log (5);
+let num = prompt ("10369");
+let d5 = (num/10000);
+let d4 = (num/1000) % 10;
+let d3 = (num/100) % 10;
+let d2 = (num/10) % 10;
+let d1 = num % 10 ;
+console.log (d5, d4, d3, d2, d1);
+
+
+
+
