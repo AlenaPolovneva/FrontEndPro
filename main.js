fe94f06d55cc17c1ac0510c70c9a065451e2f0aa
Your Name <you@example.com>
2025-02-19 22:08:26 +0200

    lec3_hw2

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
-let answer = prompt("What's your name?");
-    console.log (answer);
-let answer3 = prompt("What's your second name?");
-    console.log (answer3);
-let answer1 = prompt("How old are you?");
-    console.log (answer1);
-let answer2 = prompt("Are you male or female?");
-    console.log (answer2);
+let userName = prompt("What's your name?");
+let userSecondName = prompt("What's your second name?");
+let userAge = prompt("How old are you?");
+let userMale = prompt("Are you male or female?");
+let answer = `Hello, ${userName}, ${userSecondName}, you are ${userAge} years old, ${userMale}`;
+ alert (answer);
 
