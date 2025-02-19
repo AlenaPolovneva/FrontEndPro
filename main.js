4caca13ac38cf008c8c4e7b6d51538c8206442ee
Your Name <you@example.com>
2025-02-19 21:14:12 +0200

    lec3_hw1

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log (5);
+let years = 36;
+console.log(years);
+let name = 'July'
+console.log(name);
+let isAvailable = true;
+console.log(isAvailable);
+let isLoggedIn = false;
+console.log(isLoggedIn);
+let value = null;
+console.log(value);
+let bigNumber = 123456789123456789n;
+console.log(bigNumber);
+let mySymbol = Symbol ();
+console.log(mySymbol);
+
