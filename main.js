25c46a63ff67c11ae49e4073921ae2ae56aa37d3
Your Name <you@example.com>
2025-02-19 21:38:41 +0200

    lec3_hw2

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-console.log (5);
+let answer = prompt("What's your name?");
+    console.log (answer);
+let answer3 = prompt("What's your second name?");
+    console.log (answer3);
+let answer1 = prompt("How old are you?");
+    console.log (answer1);
+let answer2 = prompt("Are you male or female?");
+    console.log (answer2);
+
