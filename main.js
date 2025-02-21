388fdceab72d54fae6da89e83cfa24b18b3a1e96
Your Name <you@example.com>
2025-02-21 22:39:23 +0200

    lec4_hw2

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log (5);
+let num = prompt ('Enter a number');
+if (num.length === 3 && !isNan(num)) {
+    let a = num [0], b = num [1], c = num [2];
+    let allSame = a === b && b === c;
+    let anySame = a === b || b === c || a === c;
+    alert(`All number are the same : ${allSame} \ Any same number : ${anySame}`);
+} else {
+    alert('Please enter a valid three-digit number');
+}
+
