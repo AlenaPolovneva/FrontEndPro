7306ec30a192614919a0a00f660b189f9a102305
Your Name <you@example.com>
2025-02-24 22:12:05 +0200

    lec4_hw3

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
-console.log (5);
+let birthYear = ("When were you born?");
+let city = ("What city does he live in?");
+let sport  = ("What is your favorite sport?");
+let currentYear = 2025;
+let age = currentYear - birthYear;
+let cityMessage;
+switch (city) {
+    case "Kyiv":
+        cityMessage = "You live the capital of Ukraine.";
+        break;
+    case "Washington":
+        cityMessage = "You live the capital of USA.";
+        break;
+    case "London":
+        cityMessage = "You live the capital of Great Britain";
+        break;
+    default:
+        cityMessage = `You live in the city ${city}.`;
+}
+console.log (`Yu are ${age} years old. ${cityMessage}.
+Your favorite sport ${sport}.`);
+
+
+
