2c8ad4c6b03a0f9bd2d2791a2f4bca6515e4432f
Your Name <you@example.com>
2025-02-24 22:37:03 +0200

    lec4_hw4

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-console.log (5);
+let numOrStr = prompt('input number or string');
+console.log(numOrStr);
+switch (true) {
+    case (numOrStr === null):
+        console.log('ви скасували');
+        break;
+    case (numOrStr.trim() === ''):
+        console.log('Empty String');
+        break;
+    case (isNaN(+numOrStr)):
+        break;
+    default :
+        console.log('OK!');
+}
+
+
