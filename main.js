47f40bd20df5530a32a4438310ad844f9245e8be
Your Name <you@example.com>
2025-02-27 21:26:48 +0200

    lec5_hw3

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-console.log (5);
+let N = prompt (" Guess the number - ");
+N = Number (N);
+if(isNaN(N)) {
+    console.log ("Please enter a valid number.");
+} else {
+    for (let i = 1; i <= 100; i ++) {
+        if (i * i > N) {
+            break ;
+        }
+        console.log (i);
+    }
+}
+
