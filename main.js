a56495a78ae4e17d9cf2201a17395a9d961d93aa
Your Name <you@example.com>
2025-02-27 22:52:15 +0200

    lec5_hw4

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
-console.log (5);
+let input = prompt ("Please enter an integer");
+let N = Number (input);
+if (isNaN(N)) {
+    console.log ("Please enter a valid number");
+} else {
+if (N > 1) {
+    let isPrime = true;
+ for (let i = 2; i < N;i ++ ) {
+     if (N % i === 0) {
+         isPrime = false;
+         break;
+     }
+ }
+ if (isPrime) {
+     console.log (N + "is a prime number.");
+ } else {
+     console.log (N + "is not a prime number.");
+ }
+ } else {
+    console.log ("The number must be greater than 1.");
+ }
+}
+
+
