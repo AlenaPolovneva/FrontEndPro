f1237922726e5843fa800036679ca66497610cd5
Your Name <you@example.com>
2025-03-03 21:45:21 +0200

    lec6_hw1

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log (5);
+function func(str, charsToRemove) {
+    let result = "";
+    for (let i = 0; i < str.length; i++) {
+        let shouldRemove  = false;
+        for (let j = 0; j < charsToRemove.length; j++) {
+            if (str[i] === charsToRemove[j]) {
+                shouldRemove = true;
+                break;
+        }
+    }
+        if (!shouldRemove) {
+            result += str[i];
+        }
+    }
+    return result;
+}
+console.log(func(" hello world", ['l', 'd']));
+
