4a92b03e1fcbb03b2e4b924b97f92ca8987e433e
Your Name <you@example.com>
2025-03-03 22:14:42 +0200

    lec6_hw2

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-console.log (5);
+function averageOfNumbers (arr) {
+    let sum = 0;
+    let count = 0;
+    for (let i =0; i < arr. length; i++){
+    if(typeof arr[i] === "number"){
+        sum += arr[i];
+        count ++ ;
+       }
+    }
+    return count > 0 ? sum / count : 0;
+}
+console.log(averageOfNumbers([10, "hello", 20, true, 30, null]));
