e8ec062c15eb5be48e375b81bdc3693e2124f0b0
Your Name <you@example.com>
2025-03-05 22:28:01 +0200

    lec7_hw3

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log (5);
+function getNumber () {
+    let count = 0;
+    while (count < 10) {
+        let userInput = prompt("Enter a number greater than 100:");
+        let number = Number (userInput);
+        if (number > 100) {
+            console.log ("Last number entered: " + number );
+            break;
+        } else {
+            alert ("The number must be greater than 100. Try again.");
+            count ++;
+        }
+    }
+}
+getNumber();
