b270b71e5c4d930305b752e75d4ddd5445462a90
Your Name <you@example.com>
2025-03-10 21:32:21 +0200

    lec8_hw1steps

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-console.log (5);
+let ladder= {
+    step : 0,
+    up: function () {// підніматиме вас на одну сходинку
+        this.step++;
+        return this;// Повертаємо сам об'єкт для можливості ланцюжкових викликів
+    },
+    down: function () {// опускатиме вас на одну сходинку
+        this.step--;
+        return this;
+    },
+    showStep: function () {// показує поточну сходинку
+        console.log (this.step);
+        return this;
+    }
+};
+ladder.up().up().down().showStep(); // 1
