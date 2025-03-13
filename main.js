ca3903073c7f464d0188d4a8d9722cdba1d34bb8
Your Name <you@example.com>
2025-03-13 22:20:29 +0200

    lec9_hw1

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log (5);
+let company = {
+    sales: [{name : "John" , salary: 1000},{name : "Alice" , salary: 600}],
+    development: {
+        web: [{name : "Peter" , salary: 2000},{name : "Alex" , salary: 1000}],
+        internals: [{name : "Jack" , salary: 1300}],
+    }
+};
+function totalSalary(department){
+    if (Array.isArray(department)) {
+        // Якщо це масив (список співробітників), тоді підсумовуємо їхні зарплати
+        return department.reduce((sum, {salary}) => sum + salary, 0);
+    }
+        // Якщо це об'єкт (департамент, який містить інші підрозділи)
+       const subDepartments = Object.values(department);
+       return subDepartments.reduce((sum, subDep) => sum + totalSalary(subDep),0)
+}
+console.log(totalSalary(company));
+
