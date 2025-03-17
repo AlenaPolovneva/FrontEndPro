6e0d62a734783ecae403fd85ca980e0fc44796ff
Your Name <you@example.com>
2025-03-17 19:03:20 +0200

    lec10_hw1

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log (5);
+const user = {
+name: "Alena",
+    age: 37,
+    location: "Kharkiv, Ukraine",
+    email: "polovneva_alena@gmail.com",
+
+getInfo () {
+    return `Name: ${this.name}\nAge: ${this.age}\nLocation: ${this.age}\nEmail: ${this.email}`
+}
+};
+console.log (user.getInfo());
