a47b7802390fd0f3c051b608726a70102b21b734
Your Name <you@example.com>
2025-03-20 21:49:18 +0200

    lec11_hw2

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-console.log (5);
+const pyfContainer = document.querySelector("Container")
+function creatPyfTable() {
+    let table = document.createElement('table');
+
+    for (let i = 1; i <= 10; i++) {
+        let row = table.insertRow();
+
+        for (let j = 1; j <= 10; j++) {
+            let cell = row.insertCell();
+            cell.textContent = i * j;
+            cell.style.padding = '10px';
+            cell.style.textAlign = 'center';
+            cell.style.border = '1px solid black';
+        }
+    }
+    document.body.appendChild(table);
+}
+creatPyfTable();
+console.log(pyfContainer);
