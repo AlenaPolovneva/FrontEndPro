4597d199845565f2870eaaaea44da0f7237a7d5b
Your Name <you@example.com>
2025-03-24 21:48:22 +0200

    lec12_hw3

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-console.log (5);
+const participantsList = document.querySelector('#participants-list');
+const addNewInput = document.querySelector('#idea-part');
+const addNewBtn = document.querySelector('#add-new');
+participantsList.addEventListener('click', (e) =>{
+if(e.target.classList.contains('delete-btn')) {
+    e.target.parentElement.remove();
+}
+    e.stopPropagation();
+})
+addNewBtn.addEventListener('click', () => {
+    const name = addNewInput.value.trim();
+
+    if(name.trim()) {
+        const li = document.createElement( 'li');
+        li.innerHTML = `${name} <button class='delete-btn'> Remove </button>`;
+        participantsList.appendChild(li);
+    }
+    addNewInput.value = '';
+})
