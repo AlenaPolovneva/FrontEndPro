e160cab8cba1af8c6a1f46c011452292b7bd9c98
Your Name <you@example.com>
2025-03-24 19:25:04 +0200

    lec12_hw2

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-console.log (5);
+const buttonContainer = document.querySelector('#buttonContainer');
+buttonContainer.addEventListener('click', (e) => {
+    if (e.target.tagName === 'BUTTON') {
+        alert('You pressed ' + e.target.textContent);
+    }
+});
