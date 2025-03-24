bd4efc68d8943e4c30a6e84fab042a4748aa57a1
Your Name <you@example.com>
2025-03-24 19:03:50 +0200

    lec12_hw1

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log (5);
+const btn1 = document.querySelector('#linkButton');
+const btn2 = document.querySelector('#innerButton');
+let savedLink = "";
+btn1.addEventListener('click', function () {
+    const input = prompt('Enter the link:');
+    if (input) {
+        savedLink = input;
+    }
+    });
+btn2.addEventListener('click', function () {
+    if (savedLink) {
+        window.location.href = savedLink;
+    } else {
+alert ('Link not established!')
+    }
+    });
+console.log (btn1);
+console.log (btn2);
