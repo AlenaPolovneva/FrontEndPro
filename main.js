5c98b216f834bf38d169fe00fc0411dc1f6fb642
Your Name <you@example.com>
2025-04-10 21:59:34 +0300

    lec17_hw3

--- main.js
+++ main.js
@@ -1,1 +1,34 @@
-console.log (5);
+class BankAccount {
+    constructor (initialBalance) {
+        this.balance = initialBalance;
+    }
+
+    getBalance () {
+        return this.balance;
+    }
+
+    deposit(amount) {
+        if(amount > 0) {
+            this.balance += amount;
+        } else {
+            console.log ('The deposit amount must be greater than 0.');
+        }
+    }
+
+    withdraw(amount) {
+        if(amount > this.balance) {
+            console.log('Insufficient funds in the account');
+        } else if(amount <= 0) {
+            console.log ('The amount to withdraw must be greater than 0.');
+        } else {
+            this.balance -= amount;
+        }
+    }
+}
+
+const account1 = new BankAccount(1000);
+console.log(account1.getBalance());
+account1.deposit(500);
+console.log(account1.getBalance());
+account1.withdraw(200);
+console.log(account1.getBalance());
