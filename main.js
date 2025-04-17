d34f5356df63bb615ea99ec992eab6ce0abcf6cc
Your Name <you@example.com>
2025-04-17 21:25:34 +0300

    lec18_hw1

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
-console.log (5);
+let actualTime = 85;
+const timerElement = document.getElementById('timer');
+let timerInterval;
+
+function renderTimer () {
+const minutes = Math.floor(actualTime / 60);
+const seconds = actualTime % 60;
+
+    let minutesText = minutes < 10 ? '0' + minutes : minutes;
+    let secondsText = seconds < 10 ? '0' + seconds : seconds;
+
+    timerElement.textContent = minutesText + ':' + secondsText;
+}
+function startTimer() {
+    timerInterval = setInterval(() => {
+     if (actualTime > 0) {
+         actualTime --;
+         renderTimer();
+     } else {
+         clearInterval(timerInterval);
+     }
+    }, 1000);
+}
+renderTimer();
+startTimer();
