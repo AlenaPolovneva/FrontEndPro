702db2ed671a3e7e7041f0b56545c43c0ae3618d
Your Name <you@example.com>
2025-04-21 20:46:41 +0300

    lec19_hw1

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
-console.log (5);
+const API = "76a1ccf827f3e0077d5cc9e183b2d57b";
+const CITY = "Kharkiv";
+
+function fetchWeather() {
+const url =  `https://api.openweathermap.org/data/2.5/weather?q=${CITY}&appid=${API}&units=metric&lang=ua`;
+    fetch(url)
+        .then(function(response) {
+            if (!response.ok) {
+                throw new Error('Download error');
+            }
+            return response.json();
+        })
+
+        .then(function(data) {
+            document.getElementById('temperature').textContent = `${data.main.temp}°C`;
+            document.getElementById('description').textContent = data.weather[0].description;
+        })
+
+        .catch(function(error) {
+            console.error('An error occurred:', error);
+            document.getElementById('temperature').textContent = '--°C';
+            document.getElementById('description').textContent = 'Failed to load data';
+        })
+}
+fetchWeather();
